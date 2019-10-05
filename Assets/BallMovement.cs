c11cb14f01542cb496c39b5a8b34e0f86fd7e499
Guy <guyshoham28@gmail.com>
2019-10-05 12:42:54 +0300

    fix playground pads movement.

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = Vector3.back * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
