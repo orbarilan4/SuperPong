206b4a384cbde28d3486f8b88c8f74825397c8ba
OrZipori <or_zipori@walla.co.il>
2019-10-07 21:20:26 +0300

    added kicking behaviour to paddles

---
+++ Assets/KickBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickBehaviour : MonoBehaviour
+{
+    public Transform player;
+    public float hitSpeed = 150f;
+
+    private bool rightKick, leftkick;
+
+    public KeyCode leftKickCode, rightKickCode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rightKick = false;
+        this.leftkick = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(rightKickCode) && !this.rightKick)
+        {
+            this.rightKick = true;
+        }
+        if (Input.GetKeyUp(rightKickCode))
+        {
+            this.rightKick = false;
+        }
+        if (Input.GetKeyDown(leftKickCode) && !this.leftkick)
+        {
+            this.leftkick = true;
+        }
+        if (Input.GetKeyUp(leftKickCode))
+        {
+            this.leftkick = false;
+        }
+
+        if (this.rightKick) 
+        {
+            player.Rotate(Vector3.up * hitSpeed * Time.deltaTime);
+        } 
+        else if (this.leftkick)
+        {
+            player.Rotate(-Vector3.up * hitSpeed * Time.deltaTime);
+        }
+        else if (!this.rightKick || !this.leftkick)
+        {
+            player.rotation = Quaternion.identity;
+        }
+    }
+}
