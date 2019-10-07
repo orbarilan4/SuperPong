ecb0422cf4d22270b066c46e510ab97f5d4d7270
Guy <guyshoham28@gmail.com>
2019-10-07 15:17:59 +0300

    invisible gates with trigger scripts when the ball hit them. the trigger also increase score and reset board

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    public Rigidbody ball;
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("OnTriggerEnter");
+        if (ball.position.z > 0)
+        {
+            //the scorer is player2
+            gameManager.Goal(2);
+        }
+        else
+        {
+            //the scorer is player1
+            gameManager.Goal(1);
+        }
+
+    }
+}
