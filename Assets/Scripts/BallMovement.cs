077eefda59f369b08e9debb1061b3b1ad6666dfb
Guy <guyshoham28@gmail.com>
2019-10-10 12:30:50 +0300

    added random velocity to the ball when the game starts

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class BallMovement : MonoBehaviour
 {
@@ -10,8 +9,15 @@ public class BallMovement : MonoBehaviour
 
     void Start()
     {
-        rb.velocity = new Vector3(1, 0, 1) * speed;
-        //rb.velocity = Vector3.back * speed;
+        int rnd = Random.Range(0, 2);
+        if (rnd == 0)
+        {
+            rb.velocity = new Vector3(0.5f, 0, 0.5f) * speed;
+        }
+        else
+        {
+            rb.velocity = new Vector3(-0.5f, 0, -0.5f) * speed;
+        }
     }
 
     void FixedUpdate()
@@ -30,17 +36,6 @@ public class BallMovement : MonoBehaviour
 
         if (collision.gameObject.tag == "Paddle")
         {
-            //float distance1 = this.transform.position.z - GameObject.Find("Player1").transform.position.z;
-            //float distance2 = this.transform.position.z - GameObject.Find("Player2").transform.position.z;
-
-            //Debug.Log(distance1);
-            //Debug.Log(distance2);
-
-            // if(collision.gameObject.name=="Player1"){
-
-            //     rb.velocity=new Vector3()
-            // }
-
             velocity.z *= -1;
             rb.velocity = velocity;
         }
