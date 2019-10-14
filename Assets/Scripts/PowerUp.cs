527e373393e9b7e6c4753153f5433785a76c17c5
Or Bar-Ilan <orbarilan4@gmail.com>
2019-10-15 02:53:40 +0300

    Elephant + Mouse on the stage

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+	public float multiplier = 2f;
+	public float duration = 1;
+	public GameObject pickupEffect;
+	public Rigidbody ball;
+	
+    void OnTriggerEnter(Collider collider)
+	{
+		if(collider.tag == "Ball")
+		{
+			Pickup(collider);
+		}
+	}
+	
+	void Pickup(Collider collider)
+	{
+		GameObject effect = Instantiate(pickupEffect, transform.position, transform.rotation) as GameObject;
+		Destroy(effect, 0.5f);
+	
+		if(this.gameObject.name == "Big")
+		{
+			Destroy(this.gameObject);
+			collider.transform.localScale *= multiplier;
+		}
+		if(this.gameObject.name == "Small")
+		{
+			Destroy(this.gameObject);
+			collider.transform.localScale /= multiplier;
+		}
+		Debug.Log(this.gameObject.name);
+	}
+}
