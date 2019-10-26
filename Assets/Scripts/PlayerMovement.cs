bb209adb2dd2593309acca85892bdfa909e5fa91
Or Bar-Ilan <orbarilan4@gmail.com>
2019-10-26 20:16:02 +0300

    Joystick works well

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private bool playerLeft, playerRight, isMoveEnabled;
-    public float speed = 10f;
-    public KeyCode leftKeyCode, rightKeyCode;
+	protected Joystick joystick;
+    private bool /*playerLeft, playerRight,*/ isMoveEnabled;
+  //  public float speed = 10f;
+  //  public KeyCode leftKeyCode, rightKeyCode;
     // Start is called before the first frame update
     void Start()
     {
-        this.playerLeft = false;
-        this.playerRight = false;
+        //this.playerLeft = false;
+//this.playerRight = false;
         this.isMoveEnabled = true;
+		joystick = FindObjectOfType<Joystick>();
     }
 
     public void enableMove()
@@ -28,7 +30,14 @@ public class PlayerMovement : MonoBehaviour
     // 'FixedUpdate' makes bugs to i changed it to 'Update'
     void Update()
     {
-        if (Input.GetKeyDown(leftKeyCode))
+		var rigidbody = GetComponent<Rigidbody>();
+		if(isMoveEnabled)
+		{
+			rigidbody.velocity = new Vector3(joystick.Horizontal * 10f + Input.GetAxis("Horizontal") * 10f,
+											 rigidbody.velocity.y,
+											 rigidbody.velocity.z);
+		}
+        /*if (Input.GetKeyDown(leftKeyCode))
         {
             this.playerLeft = true;
         }
@@ -53,6 +62,6 @@ public class PlayerMovement : MonoBehaviour
         if (this.playerRight && isMoveEnabled)
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
+        }*/
     }
 }
