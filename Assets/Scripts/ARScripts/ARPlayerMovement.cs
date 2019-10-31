077e07ebb2cd5a5d791129aeffe7ac90b61b97c4
Guy <guyshoham28@gmail.com>
2019-10-31 12:22:13 +0200

    added AR scripts

---
+++ Assets/Scripts/ARScripts/ARPlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARPlayerMovement : MonoBehaviour
+{
+    protected Joystick joystick;
+    public float speed;
+    public Vector3 currentPosition, lastPosition;
+
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+    }
+
+    void Update()
+    {
+        //Joystick Configuration
+        currentPosition = this.transform.position;
+        var rigidbody = GetComponent<Rigidbody>();
+        float xVelocity = rigidbody.velocity.x;
+        rigidbody.velocity = rigidbody.velocity + new Vector3(joystick.Horizontal * speed + Input.GetAxis("Horizontal") * speed,
+                             rigidbody.velocity.y,
+                             rigidbody.velocity.z);
+
+        lastPosition = currentPosition;
+    }
+}
